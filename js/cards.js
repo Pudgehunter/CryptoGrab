9517f1fb377f5d9c2c0359e6faa943b670e353cd
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2021-12-01 19:41:37 -0500

    NEVER USE GIT RESET IN THE LIFE

--- js/cards.js
+++ js/cards.js
@@ -7,30 +7,30 @@ const db = getFirestore(app);
 const cards = [
         {
             id: 0,
-            name: "Bitcoin (BTC)",
-            coinValue: "57297",
+            name: "Dash Coin (DASH)",
+            coinValue: "183",
             coinQuantity: "0",
             isUp: true,
             isDown: false,
-            image: "https://http2.mlstatic.com/D_NQ_NP_682739-MLA47727283112_102021-W.webp",
+            image: "https://invezz.com/wp-content/uploads/2020/06/liquid-partners-with-dash-1280x720.jpg",
         },
         {
             id: 1,
-            name: "NEO (NEO)",
-            coinValue: "38",
+            name: "Solana (SOL)",
+            coinValue: "229",
             coinQuantity: "0",
             isUp: true,
             isDown: false,
-            image: "https://http2.mlstatic.com/D_NQ_NP_682739-MLA47727283112_102021-W.webp",
+            image: "https://cdn.bitpanda.com/media/blog/posts/solana-sol-the-crypto-of-scale-and-speed-in-the-spotlight.jpg",
         },
         {
             id: 2,
-            name: "Binance Coin (BNB)",
-            coinValue: "631",
+            name: "Decred (DCR)",
+            coinValue: "107",
             coinQuantity: "0",
             isUp: true,
             isDown: false,
-            image: "https://http2.mlstatic.com/D_NQ_NP_682739-MLA47727283112_102021-W.webp",
+            image: "https://www.tecnologia.press/wp-content/uploads/2019/08/Decred-privacidad-mixnet-transacciones-740x446.jpg",
         },
         {
             id: 3,
@@ -39,7 +39,7 @@ const cards = [
             coinQuantity: "0",
             isUp: true,
             isDown: false,
-            image: ["https://http2.mlstatic.com/D_NQ_NP_682739-MLA47727283112_102021-W.webp"]
+            image: ["https://assets.cmcmarkets.com/images/1580224251_Litecoin_extra.jpg"]
         },
         {
             id: 4,
@@ -48,7 +48,7 @@ const cards = [
             coinQuantity: "0",
             isUp: true,
             isDown: false,
-            image: ["https://http2.mlstatic.com/D_NQ_NP_682739-MLA47727283112_102021-W.webp"]
+            image: ["https://bitcoin.es/wp-content/uploads/2018/10/privacy-monero-coins-1.png"]
         }
     ];
 
